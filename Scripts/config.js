c0e0aadcca62b6a2fb67e44bb966c1179fa2aaeb
biati-digital <mannuel@biati.digital>
2020-12-24 04:59:14 +0000

    Updated code

--- Scripts/config.js
+++ Scripts/config.js
@@ -10,34 +10,23 @@ const options = {
     phpcsconfig: '',
     htmltry: '',
     htmladditional: '',
+    htmlUseTabs: false,
+    htmlTabWidth: 4,
     htmlrules: '',
+    twig: '',
+    twigUseTabs: false,
+    twigTabWidth: 4,
+    twigrules: '',
+    blade: '',
+    bladeUseTabs: false,
+    bladeTabWidth: 4,
+    bladerules: ''
 };
 
 let cachedOptions = false;
 let observing = false;
 
-function observeOptionChange(options) {
-    if (observing) {
-        return false;
-    }
-
-    for (const key in options) {
-        if (options.hasOwnProperty(key)) {
-            const optionID = nova.extension.identifier + '.' + key;
-            nova.config.onDidChange(optionID, (val) => {
-                options[key] = val;
-            });
-        }
-    }
-
-    observing = true;
-}
-
-function extensionConfig() {
-    if (cachedOptions) {
-        return cachedOptions;
-    }
-
+function getGlobalConfig() {
     let extOptions = {};
     for (const key in options) {
         if (options.hasOwnProperty(key)) {
@@ -46,11 +35,77 @@ function extensionConfig() {
             extOptions[key] = opt;
         }
     }
-    cachedOptions = extOptions;
-
-    observeOptionChange(extOptions);
 
     return extOptions;
+}
+
+function getWordspaceConfig() {
+    let worksapceOptions = {};
+
+    if (nova.workspace.config.get(nova.extension.identifier + '.workspaceconfigenable') == 'disable') {
+        return worksapceOptions;
+    }
+
+    for (const key in options) {
+        const optionID = nova.extension.identifier + '.' + key;
+        const opt = nova.workspace.config.get(optionID);
+
+        if (typeof opt !== 'object') {
+            worksapceOptions[key] = opt;
+        }
+    }
+
+    return worksapceOptions;
+}
+
+function observeOptionChange(options) {
+    if (observing) {
+        return false;
+    }
+
+    for (const key in options) {
+        const optionID = nova.extension.identifier + '.' + key;
+        nova.config.onDidChange(optionID, (val) => {
+            reloadExtensionConfig(options);
+        });
+
+        nova.workspace.config.onDidChange(optionID, (val) => {
+            reloadExtensionConfig(options);
+        });
+    }
+
+    nova.workspace.config.onDidChange(nova.extension.identifier + '.workspaceconfigenable', (val) => {
+        reloadExtensionConfig(options);
+    });
+
+    observing = true;
+}
+
+function reloadExtensionConfig(options) {
+    let config = getGlobalConfig();
+    let workspaceOptions = getWordspaceConfig();
+
+    let newOptions = Object.assign({}, config, workspaceOptions);
+
+    for (const key in newOptions) {
+        options[key] = newOptions[key];
+    }
+}
+
+function extensionConfig() {
+    if (cachedOptions) {
+        return cachedOptions;
+    }
+
+    let config = getGlobalConfig();
+    let workspaceOptions = getWordspaceConfig();
+
+    config = Object.assign({}, config, workspaceOptions);
+    cachedOptions = config;
+
+    observeOptionChange(config);
+
+    return config;
 }
 
 module.exports = extensionConfig;
